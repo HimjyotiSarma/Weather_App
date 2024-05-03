c4c37b4ded7fd97f180245eea4d1fc771291c950
Himjyoti Sarma <127434307+HimjyotiSarma@users.noreply.github.com>
2024-05-03 12:17:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# Webpack Template
+
+A Webpack Template with Eslint and Prettier Configured
+
+
+
+## Development
+
+To work on the project
+
+```bash
+  npm run start or npm start
+```
+
+## Distribution
+
+To Create a Webpack Distribution File
+
+```bash
+  npm build
+```
+
+## Deployment
+
+To Deploy in git pages
+
+```bash
+  npm publish
+```
+
---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Demo Webpack App",
+      template: "./src/index.html",
+      inject: "body",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+  devServer: {
+    static: "./dist",
+    watchFiles: ["src/**/*"],
+  },
+}
