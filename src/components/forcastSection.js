175c31020b686cb5d1b1dc2da3aec82f556c858d
Himjyoti <127434307+HimjyotiSarma@users.noreply.github.com>
2024-06-08 23:10:33 +0530

    Forcast Section added

--- src/components/forcastSection.js
+++ src/components/forcastSection.js
@@ -1,6 +1,7 @@
 const forcastSection = async (weatherData, tempFormat = "celcius") => {
   const forecastContainer = document.querySelector(".forcast-Area")
-  forecastContainer.innerHTML = ""
+  forecastContainer.innerHTML =
+    "<div class='forcast-heading'>5 Day Forcast</div>"
 
   weatherData?.forecast?.forecastday.forEach((day) => {
     const dayDiv = document.createElement("div")
@@ -13,7 +14,7 @@ const forcastSection = async (weatherData, tempFormat = "celcius") => {
     } else if (weekDay === 1) {
       weekDay = "Monday"
     } else if (weekDay === 2) {
-      weekDay = "Tueday"
+      weekDay = "Tuesday"
     } else if (weekDay === 3) {
       weekDay = "Wednesday"
     } else if (weekDay === 4) {
@@ -27,16 +28,23 @@ const forcastSection = async (weatherData, tempFormat = "celcius") => {
       tempFormat === "celcius" ? day.day.mintemp_c : day.day.mintemp_f
     let maxtemp =
       tempFormat === "celcius" ? day.day.maxtemp_c : day.day.maxtemp_f
+
+    let inputDate = new Date(day.date)
+    let date = inputDate.getDate()
+    let month = inputDate.getMonth() + 1
+    let formattedDate = `${date}/${month}`
+
     dayDiv.innerHTML = `
             <div class="date-day">
-                <span class="date">${day.date}</span>
                 <span class="day">${weekDay}</span>
+                <span class="date">${formattedDate}</span>
             </div>
             <div class="temperature">
-                <span class="high">${maxtemp}°</span> / <span class="low">${mintemp}°</span>
+                <img src="https:${day.day.condition.icon}" alt="" />
+                <span class="high">${Math.floor(maxtemp)}°</span> / <span class="low">${Math.floor(mintemp)}°</span>
             </div>
             <div class="description">${day.day.condition.text}</div>
-            <div class="rain-chance">${day.day.daily_chance_of_rain}%</div>
+            <div class="rain-chance"><svg class="precip-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 16"><path fill="none" fill-rule="nonzero" stroke="#000" stroke-width=".714" d="M5.532.891c1.723.952 5.315 5.477 5.775 8.756.028 1.718-.534 3.101-1.45 4.082C8.888 14.766 7.52 15.357 6 15.357a5.532 5.532 0 0 1-3.74-1.425c-.975-.89-1.587-2.124-1.616-3.49.503-4.035 4.013-8.49 4.888-9.551Zm-1.815 7.33a.336.336 0 0 0-.025.043c-.322.62-.59 1.255-.695 2.207.012.408.143.787.358 1.111.234.352.568.641.96.839.035.017.071.021.106.017a.201.201 0 0 0 .104-.044l.01-.005-.078-.1c-.328-.415-.82-1.067-.82-1.946 0-.752.076-1.613.08-2.121Z"></path></svg>${day.day.daily_chance_of_rain}%</div>
         `
 
     forecastContainer.appendChild(dayDiv)
